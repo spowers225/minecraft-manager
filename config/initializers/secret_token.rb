6f3dda923983903756f9384db29c5c0f5e79b031
Chris Magnacca <chrismagnacca@gmail.com>
2014-01-09 16:03:27 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinecraftManager::Application.config.secret_key_base = '3a8c4b75a1c1974d0d9afc7d83955f1f468bf2642cd5c00c8616a1e1db12b993569518ec351cec1b94462a1b667c1efb7222dc0efc55d3f6c706b89ad796f133'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MinecraftManager::Application.config.session_store :cookie_store, key: '_minecraft-manager_session'
